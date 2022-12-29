079c9ba4c96c0c3d9377385984e4b161b604ec4f
Guilherme Magno Matos <115946540+guilhermedevmatos15@users.noreply.github.com>
2022-12-29 08:58:13 -0300

    Import JS

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('button#entrar');
+const inputUser = document.querySelector('input#input-username');
+const inputPassword = document.querySelector('input#input-password');
+inputUser.addEventListener('keyup', () => {
+   
+})
+inputPassword.addEventListener('keyup', () => {
+   
+})
